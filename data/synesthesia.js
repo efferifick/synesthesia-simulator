bbe5c4047ef1498a47203332d60904cc33666f77
Erick Ochoa Lopez <eochao@ualberta.ca>
2015-09-19 23:54:57 -0600

    makes code prettier

--- data/synesthesia.js
+++ data/synesthesia.js
@@ -1,82 +1,65 @@
-function mustIgnore(str) {
-  return str == null || str.match(/^\s*$/) !== null;
+function isNodeColorable(aNode) {
+  if (aNode.parentElement.tagName.toUpperCase() == "SCRIPT"
+      || aNode.parentElement.tagName.toUpperCase() == "NOSCRIPT" ) {
+    return NodeFilter.FILTER_REJECT;
+  } else if (aNode.textContent === null || aNode.textContent.match(/^\s*$/) !== null) {
+    return NodeFilter.FILTER_REJECT;
+  } else {
+    return NodeFilter.FILTER_ACCEPT;
+  }
 }
 
-function addClassToElement(element, theClass) {
-  element.className = theClass;
-}
+function colorNode(oldNode) {
+  // aNode is guaranteed to be a text node by our filter.
+  var parent = oldNode.parentNode;
+  var characters = oldNode.nodeValue.split('');
+  var newNode = document.createElement("span")
+  newNode.className = 'synsim';
+  newNode.style.display="inline";
 
-function wrapEachCharacter(textNode, tag) {
-  var text = textNode.nodeValue;
-  var parent = textNode.parentNode;
-
-  var characters = text.split('');
-  var wrapped = document.createElement("span");
-  wrapped.style.display="inline";
-  addClassToElement(wrapped, "synsim");
   characters.forEach( function(character) {
-    var element = document.createElement(tag);
-    element.style.display="inline";
-    var code = character.charCodeAt(0).toString(16);
-    addClassToElement(element, "x" + code);
+    var className = 'x' + character.charCodeAt(0).toString(16);
+    var characterElement = document.createElement('span')
+    characterElement.className = className;
     var characterNode = document.createTextNode(character);
-    element.appendChild(characterNode);
-    wrapped.appendChild(element);
+    characterElement.appendChild(characterNode);
+    newNode.appendChild(characterElement);
   });
 
-  parent.replaceChild(wrapped, textNode);
-
+  parent.replaceChild(newNode, oldNode);
 }
 
-function walkElementNode(root) {
+function colorTree(element) {
+  var filter = { acceptNode: isNodeColorable };
+  var treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, filter, false);
+  //we cannot change the node in which we are walking.
+  //we need to walk the tree at least once to get a current node.
+  treeWalker.nextNode();
+  var oldNode = treeWalker.currentNode;
+  while (treeWalker.nextNode()) {
+    var currentNode = treeWalker.currentNode;
+    colorNode(oldNode);
+    oldNode = currentNode;
+  }
+}
 
-var walker = document.createTreeWalker(root,
-                NodeFilter.SHOW_TEXT,
-                null,
-                false
-            );
-walker.nextNode();
-var old = walker.currentNode;
-var current = walker.currentNode;
-
-while (walker.nextNode()) {
-  current = walker.currentNode
-  if (old.parentElement.tagName.toUpperCase() === "SCRIPT"
-    || old.parentElement.tagName.toUpperCase() === "NOSCRIPT") {
-    old = current;
-    continue;
-  }
-  if (mustIgnore(old.textContent)) {
-    old = current;
-    continue;
-  }
-  wrapEachCharacter(old, "span");
-  old = current;
-  }
+function colorAJAX(changes) {
+  changes.forEach( function(change) {
+      for (var i = 0; i < change.addedNodes.length; i++) {
+        if (change.addedNodes[i].nodeType == Node.ELEMENT_NODE)
+          colorTree(change.addedNodes[i]);
+        else if (change.addedNodes[i].nodeType == Node.TEXT_NODE)
+          colorNode(change.addedNodes[i]);
+      }
+    });
 }
 
 var root = document.getElementsByTagName("body")[0];
-walkElementNode(root);
+colorTree(root);
+var paintor = new MutationObserver(colorAJAX);
+var config = { attributes: true,
+               childList: true,
+               characterData: true,
+               subtree: true };
 
-var target = document.body;
-
-//create an observer
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-
-    if (mutation.addedNodes.length === 0)
-      return false;
-
-    for (var i = 0; i < mutation.addedNodes.length; i++) {
-      if (mutation.addedNodes[i].nodeType === 1)
-        walkElementNode(mutation.addedNodes[i]);
-      else if (mutation.addedNodes[i].nodeType === 3)
-        wrapEachCharacter(mutation.addedNodes[i].nodeType, "span");
-    }
-  });
-});
-
-var config = { attributes: true, childList: true, characterData: true,
-subtree: true };
-
-observer.observe(target, config);
+paintor.observe(document.body, config);
